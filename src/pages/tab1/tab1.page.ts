7be8e35441a9a9060f44e584b653bc896baa9916
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-12-23 17:46:24 +0100

    Inlcuido el plugin de scaner

--- src/pages/tab1/tab1.page.ts
+++ src/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner) {}
+
+  sliderOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false
+  }
+
+  ionViewWillEnter() {
+    this.scan();
+  }
+
+  ionViewDidEnter() {
+
+  }
+
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+    console.log('Barcode data', barcodeData);
+    }).catch(err => {
+        console.log('Error', err);
+    });
+  }
 
 }
