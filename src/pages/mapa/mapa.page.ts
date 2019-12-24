001b8016cb3fd78c9c0fe6336f4dd99d5f7e31c4
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-12-24 13:13:19 +0100

    mapas 2

--- src/pages/mapa/mapa.page.ts
+++ src/pages/mapa/mapa.page.ts
@@ -27,9 +27,65 @@ export class MapaPage implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     mapboxgl.accessToken = 'pk.eyJ1IjoicmFmYS1hcmlhZG5hIiwiYSI6ImNrNGpwbTg1MjFpZWoza3F3OGYwYnNodzgifQ.JxKVxypzfEMisNLiQrh6WA';
     const  map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [this.lat, this.lng],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      antialias: true
+      });
+
+      // The 'building' layer in the mapbox-streets vector source contains building-height
+      // data from OpenStreetMap.
+    map.on('load', () => {
+        // Insert the layer beneath any symbol layer.
+        let layers = map.getStyle().layers;
+
+        let labelLayerId;
+        for (let i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+          }
+        }
+
+        map.addLayer(
+        {
+          id: '3d-buildings',
+          source: 'composite',
+          'source-layer': 'building',
+          filter: ['==', 'extrude', 'true'],
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+          'fill-extrusion-color': '#aaa',
+
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': 0.6
+          }
+        },
+        labelLayerId
+        );
     });
   }
-
 }
